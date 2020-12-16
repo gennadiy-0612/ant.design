225562c3a4cf9d9ede020b2963a13ac80497c91a
Genn <intval@i.ua>
2020-12-16 08:57:32 +0200

    first

---
+++ src/Header.js
@@ -0,0 +1,34 @@
+import h from './img/hands.png';
+import "antd/dist/antd.css";
+import './App.css';
+
+function App() {
+    return (
+        <div className="Screen1">
+            <div className="App-header ant-row demo-row">
+                <div className="App-header-first App-header-first-col ant-col-12 demo-col demo-col-1">
+                    <img src={h} className="first-col-img" alt="logo"/>
+                </div>
+                <div className="App-header-second demo-col demo-col-1">
+                    <h2 className="App-header-second-h2">
+                        WHAT WE OFFER
+                    </h2>
+                    <p className="App-p">“IDN unleashes<br/>
+                        the power of networking
+                        and industry expertise<br/>
+                        to give Private Equity infrastructure investors
+                        what they want:</p>
+                    <a
+                        className="App-post-link"
+                        href="/"
+                        rel="noopener noreferrer"
+                    >
+                        Access to bilateral deals
+                    </a>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default App;
