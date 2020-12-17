11a88a57966913001489c4addfa794477e548d59
Genn <intval@i.ua>
2020-12-17 17:27:12 +0200

    7screen-has-started-added-max-width-1920px

--- src/Screen8.js
+++ src/Screen8.js
@@ -5,10 +5,8 @@ import peoples from "./img/peoples.png";
 function App() {
     return (
         <div className="Screen8">
-            <h2 className="Screen7--h2">Do you want to learn more?</h2>
-            <button className="login invert ant-btn ant-btn-primary ant-btn-round ant-btn-lg">
-                Join now
-            </button>
+            <h2 className="Screen7--h2 S8">Do you want to learn more?</h2>
+            <button className="login invert ant-btn ant-btn-primary ant-btn-round ant-btn-lg S8">Get in Touch</button>
         </div>
     );
 }
