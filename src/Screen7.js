8a3400d36ef4688a0fb18bb2272802ab702380be
Genn <intval@i.ua>
2020-12-17 14:18:54 +0200

    7screen-has-started-added-max-width-1920px

--- src/Screen7.js
+++ src/Screen7.js
@@ -8,7 +8,7 @@ function App() {
             <h2 className="Screen7--h2">Why IDN?</h2>
             <p className="Screen7--p">Our unique platform is the first of its kind incentivizing independent Senior
                 Executives to originate deals in a structured, innovative, and transparent way, creating an environment
-                of trust, protecting & promoting the interests of all parties involved</p>
+                of trust,<br/> protecting & promoting the interests of all parties involved</p>
             <div className="back-color">
                 <p className="normal App-p center cover">We cover all infrastructure-related sectors across Europe and
                     beyond </p>
