fc9f1e344a7563ae899c0738c130917ba1b6f9a9
Genn <intval@i.ua>
2020-12-17 17:56:08 +0200

    9screen-has-started

--- src/Screen9.js
+++ src/Screen9.js
@@ -4,11 +4,52 @@ import peoples from "./img/peoples.png";
 
 function App() {
     return (
-        <div className="Screen8">
-            <h2 className="Screen7--h2">Do you want to learn more?</h2>
-            <button className="login invert ant-btn ant-btn-primary ant-btn-round ant-btn-lg">
-                Join now
-            </button>
+        <div className="Screen9">
+            <span className="idn-img"></span>
+            <ul className="foot-ul">
+                <li className="foot-li foot-li-head">
+                    Users
+                </li>
+                <li className="foot-li">
+                    Investors
+                </li>
+                <li className="foot-li">
+                    Senior Advisors
+                </li>
+            </ul>
+            <ul className="foot-ul">
+                <li className="foot-li foot-li-head">
+                    Sectors
+                </li>
+                <li className="foot-li">
+                    Sectors
+                </li>
+            </ul>
+            <ul className="foot-ul">
+                <li className="foot-li foot-li-head">
+                    About
+                </li>
+                <li className="foot-li">
+                    About Us
+                </li>
+                <li className="foot-li">
+                    Terms & Conditions
+                </li>
+            </ul>
+            <ul className="foot-ul">
+                <li className="foot-li foot-li-head">
+                    Contact
+                </li>
+                <li className="foot-li">
+                    info@infradeals.net
+                </li>
+                <li className="foot-li">
+                    + 23232323232
+                </li>
+                <li className="foot-li">
+                    <span className="in"></span>
+                </li>
+            </ul>
         </div>
     );
 }
