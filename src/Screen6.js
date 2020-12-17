3b54fc0c5dc931e05ddc1aa0816fdeafe741801b
Genn <intval@i.ua>
2020-12-17 11:13:04 +0200

    6screen-has-added

--- src/Screen6.js
+++ src/Screen6.js
@@ -5,20 +5,22 @@ import peoples from "./img/peoples.png";
 function App() {
     return (
         <div className="Screen6">
-            <div className="window2 Screen6--w2 Screen5--w2 ant-col-12">
-                <div className="dots Screen5--dots"><img src={peoples} className="illustrate Screen5-img" alt="smile man"/></div>
+            <div className="window2 Screen6--w2 Screen5--w1 ant-col-12">
+                <div className="dots Screen5--dots Screen6--dots"><img src={peoples}
+                                                                       className="illustrate Screen5-img Screen6-img"
+                                                                       alt="smile man"/></div>
             </div>
-            <div className="window1 Screen6--w2 Screen5--w1 ant-col-12">
+            <div className="window1 Screen6--w2 Screen5--w2 ant-col-12">
                 <div className="Screen5--decription">
-                    <h2 className="App-header-second-h2 Screen5--h2 head2 center">FOR SENIOR ADVISORS</h2>
-                    <h3 className="Sreen5--h3">Success fees up to<br/> 2 million Euro</h3>
-                    <div className="Screen5--description--content">
-                        <strong className="Screen5--strong">CONNECT</strong>
-                        <p className="Screen5--p">Originate deals and connect to leading investors</p>
-                        <strong className="Screen5--strong">ADVISE</strong>
-                        <p className="Screen5--p">Introduce the parties and advise through the process</p>
-                        <strong className="Screen5--strong">CLOSE</strong>
-                        <p className="Screen5--p">Earn a success fee at deal closure and join the Board</p>
+                    <h2 className="App-header-second-h2 Screen5--h2 head2 center">FOR INVESTORS</h2>
+                    <h3 className="Sreen5--h3">Access unique<br/> investment opportunities</h3>
+                    <div className="Screen5--description--content S6c">
+                        <strong className="Screen5--strong S6st">PROPRIETARY</strong>
+                        <p className="Screen5--p S6pa">Access proprietary deals originated by our Senior Advisors</p>
+                        <strong className="Screen5--strong S6st">BILATERAL</strong>
+                        <p className="Screen5--p S6pa">Negotiate and close deals on a bilateral basis</p>
+                        <strong className="Screen5--strong S6st">WIN-WIN</strong>
+                        <p className="Screen5--p S6pa">View the deal pipeline and pay only for what you get</p>
                     </div>
                     <div className="window1--button-group Screen5--button-group">
                         <div className="window1--description">
