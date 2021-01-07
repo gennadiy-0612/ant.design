bb434ad93ca5d87b1e520fa2cfd087a359574906
Genn <intval@i.ua>
2021-01-07 14:11:36 +0200

    all-fixed

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-// import MainPage from './MainPage';
-// import Page2 from './Page2';
-// import Page3 from './Page3';
-// import Page4 from './Page4';
+import MainPage from './MainPage';
+import Page2 from './Page2';
+import Page3 from './Page3';
+import Page4 from './Page4';
 import Page5 from './Page5';
 import reportWebVitals from './reportWebVitals';
 
@@ -16,10 +16,10 @@ import reportWebVitals from './reportWebVitals';
 // );
 ReactDOM.render(
     <React.StrictMode>
-        {/*<MainPage />*/}
-        {/*<Page2 />*/}
-        {/*<Page3 />*/}
-        {/*<Page4 />*/}
+        <MainPage />
+        <Page2 />
+        <Page3 />
+        <Page4 />
         <Page5 />
     </React.StrictMode>,
     document.getElementById('root')
