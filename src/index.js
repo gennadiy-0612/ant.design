3113ce89773266104a80c50dc459fecc4aa375a4
Genn <intval@i.ua>
2021-01-07 07:24:33 +0200

    4page-is-ready

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import MainPage from './MainPage';
-import Page2 from './Page2';
-import Page3 from './Page3';
-import Page4 from './Page4';
+// import MainPage from './MainPage';
+// import Page2 from './Page2';
+// import Page3 from './Page3';
+// import Page4 from './Page4';
 import Page5 from './Page5';
 import reportWebVitals from './reportWebVitals';
 
@@ -19,8 +19,8 @@ ReactDOM.render(
         {/*<MainPage />*/}
         {/*<Page2 />*/}
         {/*<Page3 />*/}
-        <Page4 />
-        {/*<Page5 />*/}
+        {/*<Page4 />*/}
+        <Page5 />
     </React.StrictMode>,
     document.getElementById('root')
 );
