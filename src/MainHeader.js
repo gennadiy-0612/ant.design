47312890e2b65a084cd5437ba6cf2e3e7995f39e
Genn <intval@i.ua>
2021-01-21 14:36:52 +0200

    sync

--- src/MainHeader.js
+++ src/MainHeader.js
@@ -1,28 +1,29 @@
 import React from 'react'
-import { Link, NavLink } from "react-router-dom"
+import {Link, NavLink} from "react-router-dom"
 
 import Images from './Images'
 
 function MainHeader(props) {
 
-  return (
-    <div id="top" className="Header">
-      <div className="Header--logo X1024-1600">
-        <Link className="logo-link" to="/"><img className="idnlogo" src={Images.logo} alt="t" /></Link>
-      </div>
-      <div className="top-horizon-menu X1024-1200 X1200-1600">
-        <nav className="main-top-nav">
-          <NavLink className="main-top-nav-anchor" to="/advisor">Senior Advisor</NavLink>
-          <NavLink className="main-top-nav-anchor" to="/investors">Investors</NavLink>
-          <NavLink className="main-top-nav-anchor" to="/sectors">Sectors</NavLink>
-          <NavLink className="main-top-nav-anchor" to="/about">About</NavLink>
-        </nav>
-        <div className="login-block">
-          <Link className="login buttonIDN back-slider top-login" to="/login">Login</Link>
+    return (
+        <div id="top" className="Header">
+            <div className="Header--logo X1024-1600">
+                <Link className="logo-link" to="/"><img className="idnlogo" src={Images.logo} alt="t"/></Link>
+            </div>
+            <input type="checkbox" className="burger-shower"/>
+            <div className="top-horizon-menu X1024-1200 X1200-1600">
+                <nav className="main-top-nav">
+                    <NavLink className="main-top-nav-anchor" to="/advisor">Senior Advisor</NavLink>
+                    <NavLink className="main-top-nav-anchor" to="/investors">Investors</NavLink>
+                    <NavLink className="main-top-nav-anchor" to="/sectors">Sectors</NavLink>
+                    <NavLink className="main-top-nav-anchor" to="/about">About</NavLink>
+                </nav>
+                <div className="login-block">
+                    <Link className="login buttonIDN back-slider top-login" to="/login">Login</Link>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  )
+    )
 }
 
 export default MainHeader
