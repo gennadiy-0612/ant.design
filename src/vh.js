e0d56f8b316a5603ea6d95f2990630139ddaa0b6
Genn <intval@i.ua>
2021-01-28 19:04:43 +0200

    fixed

--- src/vh.js
+++ src/vh.js
@@ -3,14 +3,19 @@ import "./index"
 let vhSHCH = {
     height: '',
     firstScreen: '#top + .Screen2',
+    update: function () {
+        vhSHCH.height = window.innerHeight / 100;
+        vhSHCH.wholeHeight = Math.ceil(vhSHCH.height * 100);
+        document.querySelector(vhSHCH.firstScreen).setAttribute('style', 'height:' + vhSHCH.h + 'px;');
+    },
     setVh: function () {
-        if (1024 > window.innerWidth && window.innerWidth > 320) {
-            vhSHCH.height = window.innerHeight / 100;
-            vhSHCH.wholeHeight = Math.ceil(vhSHCH.height * 100);
-            document.querySelector(vhSHCH.firstScreen).setAttribute('style', 'height:' + vhSHCH.wholeHeight + 'px;');
-        }
+        vhSHCH.w = window.innerWidth;
+        vhSHCH.h = window.innerHeight;
+        if (600 > vhSHCH.w && vhSHCH.w > 359) vhSHCH.update();
+        if (vhSHCH.h > vhSHCH.w) vhSHCH.update();
     }
 }
-window.addEventListener('orientationchange', vhSHCH.setVh);
+
 window.addEventListener('load', vhSHCH.setVh);
+window.addEventListener('resize', vhSHCH.setVh);
 export default vhSHCH;
