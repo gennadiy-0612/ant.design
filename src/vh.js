a5b34cdbcd917585d531cacce307b30a7d2f0b0c
Genn <intval@i.ua>
2021-01-29 16:37:47 +0200

    fixed

--- src/vh.js
+++ src/vh.js
@@ -11,7 +11,7 @@ let vhSHCH = {
     setVh: function () {
         vhSHCH.w = window.innerWidth;
         vhSHCH.h = window.innerHeight;
-        if ((600 > vhSHCH.w && vhSHCH.w > 359) || (vhSHCH.h > vhSHCH.w)) vhSHCH.update();
+        if ((600 > vhSHCH.w && vhSHCH.w > 359) && (vhSHCH.h > vhSHCH.w)) vhSHCH.update();
         else document.querySelector(vhSHCH.firstScreen)?.setAttribute('style','');
     }
 }
