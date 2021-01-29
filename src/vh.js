beca33e69747c0da21aff392091bc45f2783e7eb
Genn <intval@i.ua>
2021-01-29 13:14:22 +0200

    fixed

--- src/vh.js
+++ src/vh.js
@@ -4,17 +4,17 @@ let vhSHCH = {
     height: '',
     firstScreen: '#top + .Screen2',
     update: function () {
+        if(location.href)
         vhSHCH.height = window.innerHeight / 100;
         vhSHCH.wholeHeight = Math.ceil(vhSHCH.height * 100);
-        document.querySelector(vhSHCH.firstScreen).setAttribute('style', 'height:' + vhSHCH.h + 'px;');
+        document.querySelector(vhSHCH.firstScreen)?.setAttribute('style', 'height:' + vhSHCH.h + 'px;');
     },
     setVh: function () {
         vhSHCH.w = window.innerWidth;
         vhSHCH.h = window.innerHeight;
-        if (600 > vhSHCH.w && vhSHCH.w > 359) vhSHCH.update();
-        if (vhSHCH.h > vhSHCH.w) vhSHCH.update();
+        if ((600 > vhSHCH.w && vhSHCH.w > 359) || (vhSHCH.h > vhSHCH.w)) vhSHCH.update();
         else {
-            document.querySelector(vhSHCH.firstScreen).setAttribute('style', 'height:100vh;');
+            document.querySelector(vhSHCH.firstScreen)?.setAttribute('style', 'height:100vh;');
         }
     }
 }
