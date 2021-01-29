2cc6effd89217098681f9fd3e3e3a2a8539b5d19
Genn <intval@i.ua>
2021-01-29 14:20:10 +0200

    fixed

--- src/vh.js
+++ src/vh.js
@@ -12,9 +12,7 @@ let vhSHCH = {
         vhSHCH.w = window.innerWidth;
         vhSHCH.h = window.innerHeight;
         if ((600 > vhSHCH.w && vhSHCH.w > 359) || (vhSHCH.h > vhSHCH.w)) vhSHCH.update();
-        else {
-            document.querySelector(vhSHCH.firstScreen)?.setAttribute('style', 'height:100vh;');
-        }
+        if ((1023 > vhSHCH.w && vhSHCH.w > 599) || (360 > vhSHCH.w && vhSHCH.w > 319)) document.querySelector(vhSHCH.firstScreen)?.setAttribute('style', 'height:100vh;');
     }
 }
 
