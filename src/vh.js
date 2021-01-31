cc1904dc5facbc60ab2d4d1af7c83f6c7ad7706f
Genn <intval@i.ua>
2021-01-31 19:01:31 +0200

    burger-work

--- src/vh.js
+++ src/vh.js
@@ -12,25 +12,12 @@ let vhSHCH = {
         vhSHCH.w = window.innerWidth;
         vhSHCH.h = window.innerHeight;
         if ((600 > vhSHCH.w && vhSHCH.w > 359) && (vhSHCH.h > vhSHCH.w)) vhSHCH.update();
-        if (600 > window.innerWidth && window.innerWidth > 359) console.log('ok');
         else document.querySelector(vhSHCH.firstScreen)?.setAttribute('style', '');
     },
-    burger: function () {
-        document.querySelector('.burger-shower').addEventListener('click', vhSHCH.changeClass);
-    },
-    allInOne: function () {
-        vhSHCH.setVh();
-        document.querySelector('.main-top-nav-anchor').addEventListener('click', vhSHCH.burger);
-        window.addEventListener('popstate', function (e) {
-            console.log('url changed')
-        });
-        window.addEventListener('locationchange', function () {
-            console.log('location changed!');
-        });
-    }
 }
 
-window.addEventListener('load', vhSHCH.allInOne);
-window.addEventListener('resize', vhSHCH.allInOne);
+window.addEventListener('load', vhSHCH.setVh);
+window.addEventListener('orientationchange', vhSHCH.setVh);
+window.addEventListener('resize', vhSHCH.setVh;
 
 export default vhSHCH;
