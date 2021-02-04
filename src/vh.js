419a64f8877d0044f2da83929c498b133d4341c5
Genn <intval@i.ua>
2021-02-04 11:03:08 +0200

    burger-work

--- src/vh.js
+++ src/vh.js
@@ -1,6 +1,4 @@
 const vhSHCH = {
-    land: 0,
-    portrait: 0,
     setVh: function () {
         vhSHCH.w = window.innerWidth;
         vhSHCH.h = window.innerHeight;
@@ -8,16 +6,17 @@ const vhSHCH = {
             if (vhSHCH.h > vhSHCH.w) {
                 vhSHCH.height = window.innerHeight / 100;
                 vhSHCH.wholeHeight = Math.ceil(vhSHCH.height * 100);
-                return vhSHCH.wholeHeight;
+                console.log(vhSHCH.wholeHeight);
             }
         }
+        return vhSHCH.wholeHeight;
     },
     g: function () {
-        if ((!vhSHCH.land && window.screen.orientation.type === 'landscape-primary') && (vhSHCH.h < 1023)) {
+        if ((window.screen.orientation.type === 'landscape-primary') && (vhSHCH.h < 1023)) {
             document.querySelector('.First_Screen')?.setAttribute('style', '');
             vhSHCH.land = 1;
         }
-        if ((!vhSHCH.portrait && window.screen.orientation.type === 'portrait-primary') && (vhSHCH.h < 1023)) {
+        if ((window.screen.orientation.type === 'portrait-primary') && (vhSHCH.h < 1023)) {
             document.querySelector('.First_Screen')?.setAttribute('style', '');
             vhSHCH.portrait = 1;
         }
