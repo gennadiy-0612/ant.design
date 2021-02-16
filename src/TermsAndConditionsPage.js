9a6aa021e823e98c4ac5d7e29de1a8838a303921
Genn <intval@i.ua>
2021-02-16 14:07:46 +0200

    burger-work

--- src/TermsAndConditionsPage.js
+++ src/TermsAndConditionsPage.js
@@ -3,12 +3,14 @@ import {Link} from "react-router-dom"
 
 import MainHeader from './MainHeader.js'
 import MainFooter from './MainFooter.js'
+import Terms from "./Terms";
 
 function TermsAndConditionsPage(props) {
 
     return (
         <div className="page5 twofixed">
             <MainHeader/>
+            <Terms/>
             <MainFooter/>
         </div>
     )
